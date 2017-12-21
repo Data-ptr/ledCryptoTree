656f3ec6e9866f62ce165087a71b305ba6e8c4d7
Lindy <lindy.wilkins@gmail.com>
2017-12-21 13:48:00 -0500

    patterns

--- ledCryptoTree.ino
+++ ledCryptoTree.ino
@@ -1,14 +1,14 @@
 #include <Adafruit_NeoPixel.h>
 #define verticalPIN 6
-#define VerticalNUM 10
+#define VerticalNUM 700
 #define spiralPIN 7
 #define spiralNUM 10
 bool goal[] = {
   false, false, false
 };
-int ledPin = 9;
-Adafruit_NeoPixel vertical = Adafruit_NeoPixel(VerticalNUM, verticalPIN, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel spiral = Adafruit_NeoPixel(spiralNUM, spiralPIN, NEO_GRB + NEO_KHZ800);
+int ledPin = 2;
+Adafruit_NeoPixel vertical = Adafruit_NeoPixel(VerticalNUM, verticalPIN, NEO_GRB + NEO_KHZ400);
+Adafruit_NeoPixel spiral = Adafruit_NeoPixel(spiralNUM, spiralPIN, NEO_GRB + NEO_KHZ400);
 
 void setup() {
   Serial.begin(9600);
@@ -20,26 +20,25 @@ void setup() {
   spiral.show(); // Initialize all pixels to 'off'
 }
 void loop() {
-  
+
   if ((goal[0]) && (goal[1]) && (goal[2])) {
     simpleFadeIn(9, 3);
     simpleFadeOut(9, 3);
-    RunningLights(0xff,0xff,0x00, 20);
-
+    RunningLights(0xff, 0xff, 0x00, 1);
   } else if (goal[0] && goal[1] && !goal[2]) {
     simpleFadeIn(9, 20);
-      TwinkleRandom(20, 100, false);
+    TwinkleRandom(200, 1, false);
 
     simpleFadeOut(9, 20);
-      TwinkleRandom(20, 100, false);
+    TwinkleRandom(200, 1, false);
   } else if (goal[0] && !goal[1] && !goal[2]) {
     simpleFadeIn(9, 50);
     for (int i = 0; i < 10; i++) {
-      SnowSparkle(0x10, 0x10, 0x10, 20, random(10, 100));
+      SnowSparkle(0x10, 0x10, 0x10, 200, random(10, 100));
     }
     simpleFadeOut(9, 50);
     for (int i = 0; i < 10; i++) {
-      SnowSparkle(0x10, 0x10, 0x10, 20, random(10, 100));
+      SnowSparkle(0x10, 0x10, 0x10, 200, random(10, 100));
     }
   } else {
     simpleFadeIn(9, 30);
@@ -63,24 +62,102 @@ void loop() {
       goal[2] = true;
     }
     if (state == 4) {
-      CylonBounce(0xff, 0xff, 0, 4, 10, 10);
+        colorWipe(random(0, 255),random(0, 200),random(0, 200), 0);
+        colorWipe(random(0, 200),random(0, 200),random(0, 200), 0);
+        colorWipe(random(0, 200),random(0, 200),random(0, 200), 0);
     }
     if (state == 5) {
-      CylonBounce(0xff, 0, 0, 4, 10, 50);
+      for (int i = 10; i < 20; i++) {
+        RunningLights(random(0, i * 255), random(0, i * 255), random(0, i * 255), 20);
+        colorInsta(random(0, i * 100), random(0, i * 100), 0x00, 0);
+        delay(100);
+        colorInsta(0xff, random(0, i * 100), random(0, i * 100), 0);
+        delay(100);
+        colorInsta(0xff, random(0, i * 100), 0x00, 0);
+        delay(100);
+      }
     }
     if (state == 6) {
-      colorCount(0x00, 0xff, 0x00, 50, 50);
+      for (int i = 0; i < random(0, 10); i++) {
+        theaterChase(random(0, 255), random(0, 255), random(0, 255), 50);
+      }
     }
     if (state == 7) {
-      colorCount(0x00, 0xff, 0x00, 50, 100);
+      for (int i = 0; i < random(0, 1000); i++) {
+        colorInsta(0x50, 0x50, 0x50, 0);
+        delay(20);
+        colorInsta(0x00, 0x00, 0x00, 0);
+        delay(20);
+      }
     }
     if (state == 8) {
-      colorCount(0x00, 0xff, 0x00, 50, 30);
+      for (int i = 0; i < random(0, 100); i++) {
+        colorInsta(random(0, 100), random(0, 100), 0x00, 0);
+        delay(100);
+        colorInsta(random(0, 100), 0x00, random(0, 100), 0);
+        delay(random(0, 1000));
+        colorInsta(random(0, 100), 0xff, random(0, 100), 0);
+        delay(random(0, 1000));
+        colorInsta(random(0, 100), 0x30, 0x50, 0);
+      }
     }
     if (state == 9) {
-      colorCount(0x00, 0xff, 0x00, 50, 10);
+      for (int i = 0; i < random(0, 10); i++) {
+        rainbowCycle(5);
+      }
     }
   }
+}
+void theaterChase(byte red, byte green, byte blue, int SpeedDelay) {
+  for (int j = 0; j < 10; j++) { //do 10 cycles of chasing
+    for (int q = 0; q < 3; q++) {
+      for (int i = 0; i < 700; i = i + 3) {
+        setPixel(i + q, red, green, blue);  //turn every third pixel on
+      }
+      showvertical();
+
+      delay(SpeedDelay);
+
+      for (int i = 0; i < 700; i = i + 3) {
+        setPixel(i + q, 0, 0, 0);    //turn every third pixel off
+      }
+    }
+  }
+}
+void rainbowCycle(int SpeedDelay) {
+  byte *c;
+  uint16_t i, j;
+
+  for (j = 0; j < 256; j += 10) { // 5 cycles of all colors on wheel
+    for (i = 0; i < 700; i++) {
+      c = Wheel(((i * 256 / 700) + j) & 255);
+      setPixel(i, *c, *(c + 1), *(c + 2));
+    }
+    showvertical();
+    delay(SpeedDelay);
+  }
+}
+
+byte * Wheel(byte WheelPos) {
+  static byte c[3];
+
+  if (WheelPos < 85) {
+    c[0] = WheelPos * 3;
+    c[1] = 255 - WheelPos * 3;
+    c[2] = 0;
+  } else if (WheelPos < 170) {
+    WheelPos -= 85;
+    c[0] = 255 - WheelPos * 3;
+    c[1] = 0;
+    c[2] = WheelPos * 3;
+  } else {
+    WheelPos -= 170;
+    c[0] = 0;
+    c[1] = WheelPos * 3;
+    c[2] = 255 - WheelPos * 3;
+  }
+
+  return c;
 }
 void simpleFadeIn(int pin, int speedVal) {
   for (int fadeValue = 0 ; fadeValue <= 255; fadeValue += 5) {
@@ -96,11 +173,19 @@ void simpleFadeOut(int pin, int speedVal) {
   }
 }
 void colorWipe(byte red, byte green, byte blue, int SpeedDelay) {
-  for (uint16_t i = 0; i < VerticalNUM; i++) {
+  for (uint16_t i = 0; i < 700; i++) {
     setPixel(i, red, green, blue);
     showvertical();
-    delay(SpeedDelay);
+    //delay(SpeedDelay);
   }
+}
+void colorInsta(byte red, byte green, byte blue, int SpeedDelay) {
+  for (uint16_t i = 0; i < 700; i++) {
+    setPixel(i, red, green, blue);
+
+    //delay(SpeedDelay);
+  }
+  showvertical();
 }
 void colorCount(byte red, byte green, byte blue, int SpeedDelay, int percent) {
   percent = (VerticalNUM * percent) / 100;
@@ -111,60 +196,53 @@ void colorCount(byte red, byte green, byte blue, int SpeedDelay, int percent) {
   }
   Serial.println(percent);
   for (uint16_t i = 0; i < percent; i++) {
-    setPixelS(i, red, green, blue);
-    showspiral();
+    setPixel(i, red, green, blue);
+    showvertical();
     delay(SpeedDelay);
   }
 }
 void RunningLights(byte red, byte green, byte blue, int WaveDelay) {
   int Position = 0;
-  for (int i = 0; i < VerticalNUM * 2; i++)
+  for (int i = 0; i < 700; i += 20)
   {
-    Position++; // = 0; //Position + Rate;
-    for (int i = 0; i < VerticalNUM; i++) {
-      // sine wave, 3 offset waves make a rainbow!
-      //float level = sin(i+Position) * 127 + 128;
-      //setPixel(i,level,0,0);
-      //float level = sin(i+Position) * 127 + 128;
+    Position += 10; // = 0; //Position + Rate;
+    for (int i = 0; i < 700; i++) {
       setPixel(i, ((sin(i + Position) * 127 + 128) / 255)*red,
                ((cos(i + Position) * 127 + 128) / 255)*green,
                ((sin(i + Position) * 127 + 128) / 255)*blue);
     }
-
     showvertical();
-    delay(WaveDelay);
   }
 }
 void CylonBounce(byte red, byte green, byte blue, int EyeSize, int SpeedDelay, int ReturnDelay) {
-// Spiral pattern
-  for (int i = 0; i < VerticalNUM - EyeSize - 2; i++) {
-    setAllS(0, 0, 0);
-    setPixelS(i, red / 10, green / 10, blue / 10);
+  for (int i = 0; i < 700 - EyeSize - 2; i++) {
+    setAll(0, 0, 0);
+    setPixel(i, red / 10, green / 10, blue / 10);
     for (int j = 1; j <= EyeSize; j++) {
-      setPixelS(i + j, red, green, blue);
+      setPixel(i + j, red, green, blue);
     }
-    setPixelS(i + EyeSize + 1, red / 10, green / 10, blue / 10);
-    showspiral();
-    delay(SpeedDelay);
+    setPixel(i + EyeSize + 1, red / 10, green / 10, blue / 10);
+    showvertical();
+    // delay(SpeedDelay);
   }
-  delay(ReturnDelay);
+  //delay(ReturnDelay);
   for (int i = spiralNUM - EyeSize - 2; i > 0; i--) {
-    setAllS(0, 0, 0);
-    setPixelS(i, red / 10, green / 10, blue / 10);
+    setAll(0, 0, 0);
+    setPixel(i, red / 10, green / 10, blue / 10);
     for (int j = 1; j <= EyeSize; j++) {
-      setPixelS(i + j, red, green, blue);
+      setPixel(i + j, red, green, blue);
     }
-    setPixelS(i + EyeSize + 1, red / 10, green / 10, blue / 10);
-    showspiral();
-    delay(SpeedDelay);
+    setPixel(i + EyeSize + 1, red / 10, green / 10, blue / 10);
+    showvertical();
+    //delay(SpeedDelay);
   }
 
-  delay(ReturnDelay);
+  //delay(ReturnDelay);
 }
 
 // Vertical Patterns
 void TwinkleRandom(int Count, int SpeedDelay, boolean OnlyOne) {
-// Vertical Patern
+  // Vertical Patern
   setAll(0, 0, 0);
   for (int i = 0; i < Count; i++) {
     setPixel(random(VerticalNUM), random(0, 255), random(0, 255), random(0, 255));
@@ -177,36 +255,36 @@ void TwinkleRandom(int Count, int SpeedDelay, boolean OnlyOne) {
   delay(SpeedDelay);
 }
 void SnowSparkle(byte red, byte green, byte blue, int SparkleDelay, int SpeedDelay) {
-// Vertical Pattern
+  // Vertical Pattern
   setAll(red, green, blue);
 
   int Pixel = random(VerticalNUM);
   setPixel(Pixel, 0xff, 0xff, 0xff);
   showvertical();
-  delay(SparkleDelay);
+  //delay(SparkleDelay);
   setPixel(Pixel, red, green, blue);
   showvertical();
-  delay(SpeedDelay);
+  // delay(SpeedDelay);
 }
 void Sparkle(byte red, byte green, byte blue, int SpeedDelay) {
- // Vertical Pattern
+  // Vertical Pattern
   int Pixel = random(VerticalNUM);
   setPixel(Pixel, red, green, blue);
   showvertical();
   delay(SpeedDelay);
   setPixel(Pixel, 0, 0, 0);
 }
-
-void simpleWave(float rate, int cycles, int wait) {
+//simpleWave(10, 1, 10);
+void simpleWave(float rate, int cycles, int wait, int r, int g, int b) {
   // Vertical Patern
   float pos = 0.0;
   // cycle through x times
-  for (int x = 0; x < (vertical.numPixels()*cycles); x++)
+  for (int x = 0; x < (vertical.numPixels()*cycles); x += 20)
   {
     pos = pos + rate;
     for (int i = 0; i < vertical.numPixels(); i++) {
       float level = sin(i + pos) * 127 + 128;
-      vertical.setPixelColor(i, (int)level, 255, 0);
+      vertical.setPixelColor(i, (int)level, g, b);
     }
     vertical.show();
     delay(wait);
